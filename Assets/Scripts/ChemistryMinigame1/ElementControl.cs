756d7ae9097870003d8e02ff8d4a5676af7a303b
CarmineHEX <114857362+CarmineHEX@users.noreply.github.com>
2022-11-25 01:25:26 +0700

    Refactoring Version 8

--- Assets/Scripts/ChemistryMinigame1/ElementControl.cs
+++ Assets/Scripts/ChemistryMinigame1/ElementControl.cs
@@ -10,13 +10,20 @@ public class ElementControl : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     GameObject Prefab;
+    [SerializeField]
+    GameObject elecEffect;
+    [SerializeField]
+    GameObject elecEffectSphere;
     public Rigidbody2D rb;
     private Vector2 direction;
     private System.Random rnd;
     public string nameElements;
     public int indexInContainer = 0;
+    private int numberChoiceLink;
     public Mode status=Mode.MoveUnselect;
-
+    private GameObject linkElement;
+    private GameObject elSphere;
+    private GameObject el;
     void Start()
     {
         rnd = new System.Random();
@@ -27,6 +34,8 @@ public class ElementControl : MonoBehaviour, IPointerClickHandler
 
     void FixedUpdate()
     {
+        if(linkElement!=null)
+            FollowingElec(linkElement);
         if (status==Mode.MoveUnselect|| status == Mode.MoveSelect)
             rb.MovePosition(rb.position + direction.normalized * 3f * Time.fixedDeltaTime);
     }
@@ -140,7 +149,19 @@ public class ElementControl : MonoBehaviour, IPointerClickHandler
     {
         DatabaseSubstances.selectedElements[DatabaseSubstances.numberChoice].Add(gameObject);
         DatabaseSubstances.numbersInContainer++;
+        numberChoiceLink = DatabaseSubstances.numberChoice;
         indexInContainer = DatabaseSubstances.numbersInContainer;
+        for (int i = 1; i <= DatabaseSubstances.numberChoice; i++)
+        {
+            foreach (var c in DatabaseSubstances.selectedElements[i])
+            {
+                if (indexInContainer - c.GetComponent<ElementControl>().indexInContainer == 1 &&numberChoiceLink== c.GetComponent<ElementControl>().numberChoiceLink)
+                {
+                    linkElement = c.gameObject;
+                }
+            }
+        }
+        elSphere=Instantiate(elecEffectSphere, gameObject.transform);
     }
     public void DeleteElemSelect()
     {
@@ -150,33 +171,48 @@ public class ElementControl : MonoBehaviour, IPointerClickHandler
             DatabaseSubstances.selectedElements[i].Remove(gameObject);
         }
 
-            for (int i = 1; i <= DatabaseSubstances.numberChoice; i++)
+        for (int i = 1; i <= DatabaseSubstances.numberChoice; i++)
+        {
+            foreach (var c in DatabaseSubstances.selectedElements[i])
             {
-                foreach (var c in DatabaseSubstances.selectedElements[i])
+                if (c.GetComponent<ElementControl>().indexInContainer > indexInContainer && c.GetComponent<ElementControl>().status==Mode.MoveSelect)
                 {
-                    if (c.GetComponent<ElementControl>().indexInContainer > indexInContainer && c.GetComponent<ElementControl>().status==Mode.MoveSelect)
-                    {
-                        c.GetComponent<ElementControl>().indexInContainer--;
-                    }
+                    c.GetComponent<ElementControl>().indexInContainer--;
                 }
             }
+        }
+        numberChoiceLink = default(int);
+        Destroy(elSphere);
     }
 
 //перемещает элементы в контейнер 
     public IEnumerator MoveToContainer(Vector2 pos1, Vector2 pos2, Vector2 pos3, float time1, float time2, float time3)
     {
-        RemoveRotation();
-        yield return StartCoroutine(MoveElement(pos1, time1));
+        //RemoveRotation();
+        yield return StartCoroutine(MoveElement(pos1, time1+indexInContainer));
         yield return StartCoroutine(MoveElement(pos2, time2));
+        Destroy(elSphere);
         yield return StartCoroutine(MoveElement(pos3 + new Vector2(indexInContainer, 0f), time3));
         yield return status = Mode.StaticFluctuations;
         yield return AcceptButton.isButtonAccept = false;
+        for (int i = 1; i <= DatabaseSubstances.numberChoice; i++)
+        {
+            foreach (var c in DatabaseSubstances.selectedElements[i])
+            {
+                if (indexInContainer - c.GetComponent<ElementControl>().indexInContainer == 1)
+                {
+                    linkElement = c.gameObject;
+                    el=Instantiate(elecEffect, gameObject.transform);
+                }
+            }
+        }
         PerformStatus(status);
     }
  //убираем элемент из контейнера
     public IEnumerator MoveFromContainer(Vector2 pos1, Vector2 pos2, Vector2 pos3, float time1, float time2)
     {
         DeleteElemSelect();
+        Destroy(el);
         yield return StartCoroutine(MoveElement(pos1+new Vector2(gameObject.transform.position.x,0f), time1));
         gameObject.transform.position = pos2;
         yield return StartCoroutine(MoveElement(pos3, time2));
@@ -225,35 +261,45 @@ public class ElementControl : MonoBehaviour, IPointerClickHandler
         }
     }
 
-
-//colorswitch
-    private void SelectColor(GameObject element)
+    public void FollowingElec(GameObject linkElement)
     {
-        StartCoroutine(GradualChangeBlack(element));
-    }
-    private void OriginColor(GameObject element)
-    {
-        StartCoroutine(GradualChangeOrigin(element));
-    }
-
-    private IEnumerator GradualChangeBlack(GameObject element)
-    {
-        float x = 1;
-        while (x > 0)
+        if (linkElement.GetComponent<ElementControl>().numberChoiceLink == numberChoiceLink)
         {
-            element.GetComponent<Image>().color = new Color(x, x, x, 1);
-            x -= 0.01f;
-            yield return null;
+            Vector2 dirBetweenElements;
+            dirBetweenElements = gameObject.transform.position - linkElement.transform.position;
+            var angle = Mathf.Atan2(dirBetweenElements.y, dirBetweenElements.x) * Mathf.Rad2Deg;
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, angle);
+            linkElement.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
     }
-    private IEnumerator GradualChangeOrigin(GameObject element)
-    {
-        float x = 0;
-        while (x < 1)
-        {
-            element.GetComponent<Image>().color = new Color(x, x, x, 1);
-            x += 0.01f;
-            yield return null;
-        }
-    }
+   // //colorswitch
+    //private void SelectColor(GameObject element)
+    //{
+    //    StartCoroutine(GradualChangeBlack(element));
+    //}
+    //private void OriginColor(GameObject element)
+    //{
+    //    StartCoroutine(GradualChangeOrigin(element));
+    //}
+
+    //private IEnumerator GradualChangeBlack(GameObject element)
+    //{
+    //    float x = 1;
+    //    while (x > 0)
+    //    {
+    //        element.GetComponent<Image>().color = new Color(x, x, x, 1);
+    //        x -= 0.01f;
+    //        yield return null;
+    //    }
+    //}
+    //private IEnumerator GradualChangeOrigin(GameObject element)
+    //{
+    //    float x = 0;
+    //    while (x < 1)
+    //    {
+    //        element.GetComponent<Image>().color = new Color(x, x, x, 1);
+    //        x += 0.01f;
+    //        yield return null;
+    //    }
+    //}
 }
